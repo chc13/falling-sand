1f088e3721c8739d2d09d52b772f24e9184da14c
chc13 <90738243+chc13@users.noreply.github.com>
2024-06-07 17:49:50 -0700

    getting started

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
 function setup() {
   // put setup code here
+  createCanvas(400, 400);
+  background(220);
 }
 
 function draw() {
   // put drawing code here
+  /* background(220); */
+  /* ellipse(50, 50, 80, 80); */
+  /* rect(50, 50, 20, 20); */
+
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
