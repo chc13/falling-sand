36e7b7722cdc2031cdb502cfb7a8357d114205a4
chc13 <90738243+chc13@users.noreply.github.com>
2024-06-11 17:00:28 -0700

    setting up SandGrain class

--- sketch.js
+++ sketch.js
@@ -1,19 +1,41 @@
+class SandGrain {
+  constructor() {
+    this._color = null;
+  }
+
+  getColor() {
+    return this._color;
+  }
+
+  setColor(color) {
+    this._color = color;
+  }
+}
+
 function setup() {
   // put setup code here
-  createCanvas(400, 400);
-  background(220);
+  frameRate(60);
+  createCanvas(800, 800);
+  background(240);
 }
 
 function draw() {
   // put drawing code here
-  /* background(220); */
+  /* clear();
+  background(240); */
   /* ellipse(50, 50, 80, 80); */
   /* rect(50, 50, 20, 20); */
 
-  if (mouseIsPressed) {
+  /*   if (mouseIsPressed) {
     fill(0);
   } else {
     fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+  } */
+  /* stroke("black");
+  ellipse(mouseX, mouseY, 80, 80); */
+
+  point(mouseX, mouseY);
+
+  stroke("red");
+  point(100, 100);
 }
